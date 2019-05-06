554a6ec411a33a72f830033a7b7e5143a8aee9cd
MmmmyPh <MmmmyPh@gmail.com>
2019-05-06 14:00:57 +0800

    尝试

---
+++ packages/utils/have/index.js
@@ -0,0 +1,35 @@
+import typeCheck from '../type-check';
+
+export const haveMarks = ( { value }, type ) => {
+	if ( value.marks.size > 0 ) {
+		return value.marks.some( mark => mark.type === type );
+	}
+
+	return false;
+};
+
+export const haveBlocks = ( { value }, type ) => {
+	if ( !typeCheck.isArray( type ) ) {
+		type = [ type ];
+	}
+	if ( value.blocks.size > 0 ) {
+		return value.blocks.some( block => type.findIndex( t => block.type === t ) !== -1 );
+	}
+
+	return false;
+};
+
+export const haveInlines = ( { value }, type ) => {
+	if ( value.inlines.size > 0 ) {
+		return value.inlines.some( inline => inline.type === type );
+	}
+
+	return false;
+};
+
+export const haveEqualedDataKeyValueInSomeBlock = ( { value }, dataKey, dataValue ) => {
+	if ( value.blocks.size > 0 ) {
+		return value.blocks.some( block => block.get( 'data' ).has( dataKey ) && block.get( 'data' ).get( dataKey ) === dataValue );
+	}
+	return false;
+};
