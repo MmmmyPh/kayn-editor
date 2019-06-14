a102ae7e7c8911112d372d7d15fc4077b8328ff1
MmmmyPh <MmmmyPh@gmail.com>
2019-06-14 13:09:07 +0800

    修复嵌套list无法重新渲染问题

--- src/serializer/html/blockRules.js
+++ src/serializer/html/blockRules.js
@@ -54,6 +54,22 @@ const blockRules = [ {
 				data: data,
 				nodes: next( el.childNodes ),
 			};
+		}else{
+			const elClsStr = el.getAttribute && el.getAttribute( 'class' );
+			if( /list-item-child/.test( elClsStr ) ) {
+				const indent = el && el.getAttribute && el.getAttribute( dataPaddingLeft ) ? parseInt( el.getAttribute( dataPaddingLeft ) ) / 3 : 0;
+				return {
+					object: 'block',
+					type: LIST_ITEM_CHILD,
+					data: {
+						className: el.getAttribute( 'class' ),
+						align: el.getAttribute( dataTextAlign ),
+						indent: indent,
+						lineHeight: el.getAttribute( dataLineHeight ),
+					},
+					nodes: next( el.childNodes ),
+				};
+			}
 		}
 	},
 	// Switch serialize to handle more blocks...
@@ -75,37 +91,40 @@ const blockRules = [ {
 				width: obj.data.get( 'width' ),
 				height: obj.data.get( 'height' )
 			};
+			const clsStr = obj.data.get('className') || '';
 			switch ( obj.type ) {
 				case PARAGRAPH:
-					return <p className = { obj.data.get( 'className' ) } style = { blockStyleAttrs } { ...blockAttrs }>{children}</p>;
+					return <p className = { clsStr } style = { blockStyleAttrs } { ...blockAttrs }>{children}</p>;
 				case BLOCKQUOTE:
-					return <blockquote className = { obj.data.get( 'className' ) } style = { blockStyleAttrs } { ...blockAttrs }>{children}</blockquote>;
+					return <blockquote className = { clsStr } style = { blockStyleAttrs } { ...blockAttrs }>{children}</blockquote>;
 				case HEADING_1:
-					return <h1 className = { obj.data.get( 'className' ) } style = { blockStyleAttrs } { ...blockAttrs }>{children}</h1>;
+					return <h1 className = { clsStr } style = { blockStyleAttrs } { ...blockAttrs }>{children}</h1>;
 				case HEADING_2:
-					return <h2 className = { obj.data.get( 'className' ) } style = { blockStyleAttrs } { ...blockAttrs }>{children}</h2>;
+					return <h2 className = { clsStr } style = { blockStyleAttrs } { ...blockAttrs }>{children}</h2>;
 				case HEADING_3:
-					return <h3 className = { obj.data.get( 'className' ) } style = { blockStyleAttrs } { ...blockAttrs }>{children}</h3>;
+					return <h3 className = { clsStr } style = { blockStyleAttrs } { ...blockAttrs }>{children}</h3>;
 				case HEADING_4:
-					return <h4 className = { obj.data.get( 'className' ) } style = { blockStyleAttrs } { ...blockAttrs }>{children}</h4>;
+					return <h4 className = { clsStr } style = { blockStyleAttrs } { ...blockAttrs }>{children}</h4>;
 				case HEADING_5:
-					return <h5 className = { obj.data.get( 'className' ) } style = { blockStyleAttrs } { ...blockAttrs }>{children}</h5>;
+					return <h5 className = { clsStr } style = { blockStyleAttrs } { ...blockAttrs }>{children}</h5>;
 				case HEADING_6:
-					return <h6 className = { obj.data.get( 'className' ) } style = { blockStyleAttrs } { ...blockAttrs }>{children}</h6>;
+					return <h6 className = { clsStr } style = { blockStyleAttrs } { ...blockAttrs }>{children}</h6>;
 				case TABLE:
-					return <table className = { obj.data.get( 'className' ) } style = { blockStyleAttrs } { ...blockAttrs }>{children}</table>;
+					return <table className={`kayn__table ${clsStr}`} style={blockStyleAttrs} {...blockAttrs}>{children}</table>;
 				case TABLE_ROW:
-					return <tr className = { obj.data.get( 'className' ) } style = { blockStyleAttrs } { ...blockAttrs }>{children}</tr>;
+					return <tr className={`kayn__tr ${clsStr}`} style={blockStyleAttrs} {...blockAttrs}>{children}</tr>;
 				case TABLE_CELL:
-					return <td className = { obj.data.get( 'className' ) } style = { blockStyleAttrs } { ...blockAttrs }>{children}</td>;
+					return <td className={`kayn__td ${clsStr}`} style={blockStyleAttrs} {...blockAttrs}>{children}</td>;
 				case OL_LIST:
-					return <ol className = { obj.data.get( 'className' ) } style = { blockStyleAttrs } { ...blockAttrs }>{children}</ol>;
+					return <ol className={`ordered-list kayn__ol ${clsStr}`} style={blockStyleAttrs} {...blockAttrs}>{children}</ol>;
 				case UL_LIST:
-					return <ul className = { obj.data.get( 'className' ) } style = { blockStyleAttrs } { ...blockAttrs }>{children}</ul>;
+					return <ul className={`unordered-list kayn__ul ${clsStr}`} style={blockStyleAttrs} {...blockAttrs}>{children}</ul>;
 				case LIST_ITEM:
-					return <li className = { obj.data.get( 'className' ) } style = { blockStyleAttrs } { ...blockAttrs }>{children}</li>;
+					return <li className={`list-item kayn__li ${clsStr}`} style={blockStyleAttrs} {...blockAttrs}>{children}</li>;
+				case LIST_ITEM_CHILD:
+					return <div className={`list-item-child kayn__li-child ${clsStr}`} style={blockStyleAttrs} {...blockAttrs}>{children}</div>;
 				case IMAGE:
-					return <div className = { obj.data.get( 'className' ) } style = { blockStyleAttrs } { ...blockAttrs }>
+					return <div className = { clsStr } style = { blockStyleAttrs } { ...blockAttrs }>
 						<img src = { obj.data.get( 'src' ) } width = { obj.data.get( 'width' ) } height = { obj.data.get( 'height' ) } { ...blockAttrs } />
 					</div>;
 				// case 'code':
@@ -115,7 +134,7 @@ const blockRules = [ {
 				// 		</pre>
 				// 	);
 				default:
-					return <p className = { obj.data.get( 'className' ) } style = { blockStyleAttrs } { ...blockAttrs }>{children}</p>;
+					return <p className = { clsStr } style = { blockStyleAttrs } { ...blockAttrs }>{children}</p>;
 			}
 		}
 	},
